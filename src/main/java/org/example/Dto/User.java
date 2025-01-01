637c61d62b15c4eb4e0a90070d63236230b0ace2
Managu <kuon.manaku@gmail.com>
2025-01-01 18:39:13 +0900

    2025/01/01 Add Check Log

--- src/main/java/org/example/Dto/User.java
+++ src/main/java/org/example/Dto/User.java
@@ -57,43 +57,73 @@ public class User {
     public String updateUser;
 
     // getter-setter
-    public void setUserListNo(int userListNo){ this.userListNo = userListNo; }
+    public void setUserListNo(int userListNo){
+        System.out.println("UserListNo() call succeeded");
+        this.userListNo = userListNo;
+    }
 
     public int getUserListNo() { return this.userListNo; }
 
-    public void setUserId(String userId){ this.userId = userId; }
+    public void setUserId(String userId){
+        System.out.println("UserId() call succeeded");
+        this.userId = userId;
+    }
 
     public String getUserId() { return this.userId; }
 
-    public void setUserName(String userName){ this.userName = userName; }
+    public void setUserName(String userName){
+        System.out.println("UserName() call succeeded");
+        this.userName = userName;
+    }
 
     public String getUserName() { return this.userName; }
 
-    public void setUserPw(String userPw){ this.userPw = userPw; }
+    public void setUserPw(String userPw){
+        System.out.println("UserPw() call succeeded");
+        this.userPw = userPw;
+    }
 
     public String getUserPw() { return this.userPw; }
 
-    public void setUserEmail(String userEmail){ this.userEmail = userEmail; }
+    public void setUserEmail(String userEmail){
+        System.out.println("UserEmail() call succeeded");
+        this.userEmail = userEmail;
+    }
 
     public String getUserEmail() { return this.userEmail; }
 
-    public void setFirstTimePwFlg(String firstTimePwFlg){ this.firstTimePwFlg = firstTimePwFlg; }
+    public void setFirstTimePwFlg(String firstTimePwFlg){
+        System.out.println("FirstTimePwFlg() call succeeded");
+        this.firstTimePwFlg = firstTimePwFlg;
+    }
 
     public String getFirstTimePwFlg() { return this.firstTimePwFlg; }
 
-    public void setCreateDatetime(LocalTime createDatetime) { this.createDatetime = createDatetime; }
+    public void setCreateDatetime(LocalTime createDatetime) {
+        System.out.println("CreateDatetime() call succeeded");
+        this.createDatetime = createDatetime;
+    }
 
     public LocalTime getCreateDatetime(String createDatetime) { return this.createDatetime; }
 
-    public void setCreateUser(String createUser) { this.createUser = createUser; }
+    public void setCreateUser(String createUser) {
+        System.out.println("CreateUser() call succeeded");
+        this.createUser = createUser;
+    }
 
     public String getCreateUser(String createUser) { return this.createUser; }
 
-    public void setUpdateDatetime(LocalTime updateDatetime) { this.updateDatetime = updateDatetime; }
+    public void setUpdateDatetime(LocalTime updateDatetime) {
+        System.out.println("UpdateDatetime() call succeeded");
+        this.updateDatetime = updateDatetime;
+    }
 
     public LocalTime getUpdateDatetime(String updateDatetime) { return this.updateDatetime; }
 
-    public void setUpdateUser(String updateUser) { this.updateUser = updateUser; }
+    public void setUpdateUser(String updateUser) {
+        System.out.println("UpdateUser() call succeeded");
+        this.updateUser = updateUser;
+    }
 
     public String getUpdateUser(String updateUser) { return this.updateUser; }
 
